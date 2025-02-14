2f8cc1bb2972d04e5dc231358b7113f741e99175
Michael <>
2025-02-14 18:40:43 +0100

    Make font configurable

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,4 +1,4 @@
-import java.awt.{Font, Color}
+import java.awt.{Font, GraphicsEnvironment, Color}
 import java.awt.image.{BufferedImage, RenderedImage}
 import java.io.File
 import javax.imageio.ImageIO
@@ -15,24 +15,32 @@ import scala.util.{Try, Success, Failure}
       .zipWithIndex
       .map(LetterConfig.New(_))
   for (letter_config <- letter_configs)
-    val image = letter_config.make_image(config.size)
+    val image = letter_config.make_image(config)
     val image_path = s"${config.path}/${letter_config.index}.png"
     write_image(image, image_path)
+
+  // Available fonts
+  // println(GraphicsEnvironment.getLocalGraphicsEnvironment().getAvailableFontFamilyNames().toList)
 
 case class Config(
     size: Int,
     letters: String,
+    font: Font,
     colors: List[String],
     path: String
 )
 
 def read_config(path: String) =
   val json = ujson.read(os.read(os.pwd / path))
+
+  val size = Try(json("size").num.toInt).getOrElse(400)
+
   Config(
-    Try(json("size").num.toInt).getOrElse(400),
+    size,
     Try(json("text").str).getOrElse("LLB"),
+    Font(Try(json("font").str).getOrElse("Monospaced"), Font.PLAIN, (size * 1.15).toInt),
     Try(json("colors").arr.map(_.str).toList).getOrElse(List("magenta", "0xff00ff", "fuchsia")),
-    Try(json("path").str).getOrElse("images")
+    Try(json("path").str).getOrElse("images"),
   )
 
 case class LetterConfig(
@@ -40,12 +48,12 @@ case class LetterConfig(
     color: Color,
     index: Int
 ) {
-  def make_image(size: Int) =
+  def make_image(config: Config) =
     // See also: https://otfried.org/scala/drawing.html
     // https://coderanch.com/t/753449/java/Font-metrics-centering-character-box
 
     val text = letter.toString()
-    val canvas = new BufferedImage(size, size, BufferedImage.TYPE_INT_RGB)
+    val canvas = new BufferedImage(config.size, config.size, BufferedImage.TYPE_INT_RGB)
     val graphics = canvas.createGraphics()
 
     // Create white canvas with black border
@@ -55,9 +63,8 @@ case class LetterConfig(
     graphics.fillRect(1, 1, canvas.getWidth() - 2, canvas.getHeight() - 2)
 
     // Write letter
-    val font = Font("Arial", Font.PLAIN, (size * 1.15).toInt)
     graphics.setColor(color)
-    graphics.setFont(font)
+    graphics.setFont(config.font)
     val metrics = graphics.getFontMetrics()
     val box = metrics.getStringBounds(text, graphics)
     val width = ((canvas.getWidth() - box.getWidth()) / 2).toFloat
